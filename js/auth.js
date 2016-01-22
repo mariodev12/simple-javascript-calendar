e7789a904d65de0d884e23190826ac7b5750ab70
Mario <mdiez12@gmail.com>
2016-01-22 23:04:33 +0100

    Commit message

--- js/auth.js
+++ js/auth.js
@@ -1,16 +1,32 @@
 'use strict';
 
 var Auth = (function () {
-  Parse.initialize('app id','javascript key');
+  const APP_ID = '';
+  const JS_KEY = '';
 
-  var register = function(e){
+  Parse.initialize(APP_ID,JS_KEY);
+
+  var login = function(){
+    var form = document.getElementById('form-login');
+
+    Parse.User.logIn(form.username.value,form.password.value, {
+      success: function(user){
+        form.submit();
+        console.log('Wellcome '+user);
+      },
+      error: function(user, error){
+        console.log('not match');
+      }
+    });
+  };
+
+  var register = function(){
+    //event.preventDefault();
     var user = new Parse.User();
-
     var form = document.getElementById('form-register');
 
     var nameRegister = form.username.value;
     var emailRegister = form.email.value;
-    //var passwordRegister = //register.password.value;
     var password2Register = form.password2.value;
 
     user.set("username", nameRegister);
@@ -19,8 +35,8 @@ var Auth = (function () {
 
     user.signUp(null, {
       success: function(user){
-        form.submit();
         console.log("Signed up user");
+        form.submit();
       },
       error: function(user, eror){
         console.log("error while signing up");
@@ -28,10 +44,8 @@ var Auth = (function () {
     });
   };
   return {
-    register: register
+    register: register,
+    login: login
   };
-
+  //$('form-register').submit(false);
 })(window);
-Auth.register(function(e){
-  e.preventDefault();
-});

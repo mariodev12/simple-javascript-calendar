9494fb954814ce873c0387b2131330adadf29125
Mario <mdiez12@gmail.com>
2016-01-24 23:15:09 +0100

    Commit message

--- rest-api-server/app.js
+++ rest-api-server/app.js
@@ -1,59 +1,37 @@
-var express = require('express'),
-    http = require('http'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    mongoose = require('mongoose'),
-    api = 'http://api.tvmaze.com/schedule/full';
-var async = require('async');
-var tvmazeResponse;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8500;
+var request = require('request');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-var router = express.Router();
-
-app.get('/', function(req, res, next){
-  async.waterfall([
-      function(callback){
-        req.get(api, function(error, response, body){
-          var events = JSON.stringify(body);
-          _.each(events, function(events){
-            console.log(events._embedded.show.name)
-          });
-          /* var show = new tvShow({
-            title:
-          }) */
-        });
-      }
-  ], function(err, events){
-    if(err) next(err);
-    events.save(function(err){
-      if(err){
-        if(err.code == 11000){
-          return res.send(409);
-        }
-        return next(err);
-      }
-      res.send(200);
-    });
-  });
+var options = {
+  url:'http://api.tvmaze.com/schedule/full',
+  method: 'GET'
+}
+app.get('/', function(req, res){
+  request('http://api.tvmaze.com/schedule/full', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.json(JSON.parse(body)); // Show the HTML for the Google homepage.
+    }
+  })
 });
 
-router.get('/', function(req, res){
-  res.send(body);
+app.get('/joke/random', function(req, res){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  var index = Math.floor(Math.random() * jokes.length);
+  var randomQuote = jokes[index];
+  res.json(randomQuote);
 });
 
-app.use(router);
+app.get('/joke/:id', function(req, res) {
+  if(jokes.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No joke found');
+  }
+var q = jokes[req.params.id];
+  res.json(q);
+});
 
-mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
-  if(err) {
-    console.log('ERROR: connecting to Database. ' + err);
-  } else {
-    console.log('Connected!');
-   }
-  app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
-  });
+app.listen(port, function(){
+  console.log('Listening in on ' + port);
 });

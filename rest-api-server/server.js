d483478820ed9beb73ae79c734f69012855a6627
Mario <mario@MacBook-Pro-de-Mario.local>
2016-02-29 21:23:47 +0100

    commit new

--- rest-api-server/server.js
+++ rest-api-server/server.js
@@ -3,6 +3,7 @@ var app = express();
 var port = process.env.PORT || 8500;
 var request = require('request');
 var responseJSON;
+var traktResponse;
 var result = [];
 var parsedDataEvents = [];
 var trakt = [];
@@ -21,7 +22,7 @@ request({
   headers: {
     'Content-Type': 'application/json',
     'trakt-api-version': '2',
-    'trakt-api-key': ''
+    'trakt-api-key': 'ae12cefd1d77b153f248bc9b20d7d0cdfbf21d2754fa7f25bfc23c53a41851e5'
   }}, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var traktResponse = JSON.parse(body);
@@ -32,23 +33,30 @@ request({
     }
 });
 
-request(options, function (error, response, body) {
+request({
+  method: 'GET',
+  url: 'https://api-v2launch.trakt.tv/calendars/all/shows/2016-01-01/366',
+  headers: {
+    'Content-Type': 'application/json',
+    'trakt-api-version': '2',
+    'trakt-api-key': 'ae12cefd1d77b153f248bc9b20d7d0cdfbf21d2754fa7f25bfc23c53a41851e5'
+  }}, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    responseJSON = JSON.parse(body);
-    for (var i = 0; i < responseJSON.length; i++) {
-      parsedDataEvents.push({title: responseJSON[i]._embedded.show.name,
-        start: responseJSON[i].airdate, episode: responseJSON[i].season+"x"+responseJSON[i].number });
+    traktResponse = JSON.parse(body);
+    for (var i = 0; i < traktResponse.length; i++) {
+      trakt.push({title: traktResponse[i].show.title, start: traktResponse[i].first_aired.substring(0,10),
+      episode: traktResponse[i].episode.season+"x"+traktResponse[i].episode.number });
     }
     var groups = {};
-    for (var i = 0; i < responseJSON.length; i++) {
-      var item = responseJSON[i];
+    for (var i = 0; i < traktResponse.length; i++) {
+      var item = traktResponse[i];
 
-      if(!groups[item.airdate]){
-        groups[item.airdate] = [];
+      if(!groups[item.first_aired.substring(0,10)]){
+        groups[item.first_aired.substring(0,10)] = [];
       }
-      groups[item.airdate].push({
-        title: item._embedded.show.name,
-        episode: responseJSON[i].season+"x"+responseJSON[i].number
+      groups[item.first_aired.substring(0,10)].push({
+        title: item.show.title,
+        episode: traktResponse[i].episode.season+"x"+traktResponse[i].episode.number
       });
     }
     for(var x in groups) {
